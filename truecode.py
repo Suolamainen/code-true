ea35b1f18428bcbc8b6a03cc96e7b0312e806de0
Suolamainen <93546777+Suolamainen@users.noreply.github.com>
2022-05-16 08:38:54 +0300

    Create truecode.py

---
+++ truecode.py
@@ -0,0 +1,9 @@
+printer("does the print"):
+    If code == True:
+        true = False
+        Write"(True!)"
+    elseif code == False:
+        dontprint("True!)"
+        else print"("False:)
+    elsedo:
+        int("print(3 +5 =(5+3))")
